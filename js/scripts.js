d83cef9e715bf0962fd11fdd41c8bc14c05a86f8
Anh Le <maianh_tom@yahoo.com.vn>
2020-09-13 22:03:44 +0300

    form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,97 @@
-var registeredUsers = []; // this array stores valid usernames until the next pageload
+var registeredUsers = ["Hoang", "Maija", "Anne", "Jarmo"]; // this array stores valid usernames until the next pageload
 
-function validateForm(e){
-    e.preventDefault(); // stop the submit button from refreshing the page
-    console.log('validating....');
+if (registeredUsers.length > 5) {
+  registeredUsers.shift();
+}
 
-    console.log('user name: ' + validateUsername());
-    console.log('email: ' + validateEmail());
-    console.log('password: ' + validatePassword());
+function validateForm(e) {
+  e.preventDefault(); // stop the submit button from refreshing the page
+  console.log("validating....");
 
-    if (validateUsername() && validateEmail() && validatePassword()) {
-        var _newUser = getUserName();   
-        // add code to update registeredUsers array with new user and call render function
-        // TODO
-        document.registration.reset(); // reset form input fields
+  console.log("user name: " + validateUsername());
+  console.log("email: " + validateEmail());
+  console.log("password: " + validatePassword());
+
+  if (validateUsername() && validateEmail() && validatePassword()) {
+    var _newUser = getUserName();
+    // add code to update registeredUsers array with new user call render function
+    // TODO
+    return registeredUsers.push(_newUser);
+    if (registeredUsers.length > 5) {
+      registeredUsers.shift();
     }
+    //call render function
+    document.getElementById("registered-user").innerHTML = "";
+    renderRegisteredUsers();
+
+    document.registration.reset(); // reset form input fields
+  }
 }
 
 function renderRegisteredUsers() {
-    registeredUsers.forEach(function(registeredUser){
-        var _newUser = document.createElement('li'); 
-        _newUser.innerHTML = registeredUser;
-        document.getElementById('registered-users').appendChild(_newUser);
-    });
+  registeredUsers.forEach(function (registeredUser) {
+    var _newUser = document.createElement("li");
+    _newUser.innerHTML = registeredUser;
+    document.getElementById("registered-users").appendChild(_newUser);
+  });
 }
 
 /**
  * this function supposely validates submitted username
  * @returns [Boolean] true when valid, false otherwise
  */
-function validateUsername(){
-    var _userName = getUserName();
-    
-    return !checkSpace(_userName);
+function validateUsername() {
+  var _userName = getUserName();
+
+  return !checkSpace(_userName);
 }
 
 /**
  * this function supposely validates submitted email
  * @returns [Boolean] true when valid, false otherwise
  */
-function validateEmail(){
-    var _email = getEmail();
+function validateEmail() {
+  var _email = getEmail();
 
-    if (checkSpace(_email) === true) {
-        return false;
-    }
+  if (checkSpace(_email) === true) {
+    return false;
+  }
 
-    // check for @
-    var atSymbol = _email.indexOf('@');
-    if(atSymbol < 1) {
-        return false;
-    }
+  // check for @
+  var atSymbol = _email.indexOf("@");
+  if (atSymbol < 1) {
+    return false;
+  }
 
-    // check if there is a dot located less than 2 symbols away from the @ sign
-    var dot = _email.indexOf('.');
-    if(dot <= atSymbol + 2) {
-        return false;
-    } 
+  // check if there is a dot located less than 2 symbols away from the @ sign
+  var dot = _email.indexOf(".");
+  if (dot <= atSymbol + 2) {
+    return false;
+  }
 
-    // check that the dot is not at the end
-    if (dot === _email.length - 1){
-        return false;
-    } 
+  // check that the dot is not at the end
+  if (dot === _email.length - 1) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 /**
  * this function supposely validates submitted password
- * if password and confirmPassword do not match, return false 
- * 
+ * if password and confirmPassword do not match, return false
+ *
  * @returns [Boolean] true when valid, false otherwise
  */
 function validatePassword() {
-    var _password = getPassword();
-    var _confirmPassword = getConfirmPassword();
+  var _password = getPassword();
+  var _confirmPassword = getConfirmPassword();
 
-    if (_password !== _confirmPassword) {
-        return false;
-    }
+  if (_password !== _confirmPassword) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 /**
@@ -89,32 +101,70 @@ function validatePassword() {
  * @returns [Boolean] true when valid, false otherwise
  */
 function checkSpace(sample) {
-    return sample === '' || sample.indexOf(' ') > -1;
+  return sample === "" || sample.indexOf(" ") > -1;
 }
 
 /**
  * this function looks under the form with name "registration"
  * look under the "username" input field and returns the value of it
  * returns nothing if no value is found
- * 
+ *
  * @returns [Boolean] true when valid, false otherwise
  */
 function getUserName() {
-    if (typeof(document.registration.username.value) === 'undefined') {
-        return '';
-    } else {
-        return document.registration.username.value;
-    }   
+  if (typeof document.registration.username === "undefined") {
+    return "";
+  } else {
+    return document.registration.username.value;
+  }
 }
+
+/**
+ * this function looks under the form with name "registration"
+ * look under the "email" input field and returns the value of it
+ * returns nothing if no value is found
+ *
+ * @returns [Boolean] true when valid, false otherwise
+ */
 
 function getEmail() {
-    // TODO
+  // TODO
+  if (typeof document.registration.email === "undefined") {
+    return "";
+  } else {
+    return document.registration.email.value;
+  }
 }
 
+/**
+ * this function looks under the form with name "registration"
+ * look under the "Password" input field and returns the value of it
+ * returns nothing if no value is found
+ *
+ * @returns [Boolean] true when valid, false otherwise
+ */
 function getPassword() {
-    // TODO
+  // TODO
+  if (typeof document.registration.password === "undefined") {
+    return "";
+  } else {
+    return document.registration.password.value;
+  }
 }
+
+/**
+ * this function looks under the form with name "registration"
+ * look under the "ConfirmPassword" input field and returns the value of it
+ * returns nothing if no value is found
+ *
+ * @returns [Boolean] true when valid, false otherwise
+ */
 
 function getConfirmPassword() {
-    // TODO
+  // TODO
+  if (typeof document.registration.password_confirm === "undefined") {
+    return "";
+  } else {
+    return document.registration.password_confirm.value;
+  }
 }
